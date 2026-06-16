9108806a2350ee63ecf2f2a1b92fb1b7faa88b46
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-16 23:37:15 +0700

    video 35

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import'./components/todo/todo.css';
-import TodoNew from './components/todo/TodoNew';
-import TodoData from './components/todo/TodoData';
-import reactLogo from './assets/react.svg'
+import "./components/todo/todo.css";
+import TodoNew from "./components/todo/TodoNew";
+import TodoData from "./components/todo/TodoData";
+import reactLogo from "./assets/react.svg";
 
 const App = () => {
     const name = "props";
     const age = 25;
     const data = {
-        address: 'hanoi',
-        country: 'vietnam'
-    }
+        address: "hanoi",
+        country: "vietnam",
+    };
+
+    const addNewTodo = (name) => {
+        alert(`Call me ${name}`);
+    };
 
     return (
         <div className="todo-container">
             <div className="todo-title">Todo List</div>
-            <TodoNew/>
-            <TodoData
-            name={name}
-            age={age}
-            data={data}
+            <TodoNew 
+            addNewTodo={addNewTodo}
+             />
+            <TodoData 
+            name={name} 
+            age={age} 
+            data={data} 
             />
-            <div className='todo-image'>
-                <img src={reactLogo} alt=""  className='logo'/>
+            <div className="todo-image">
+                <img src={reactLogo} alt="" className="logo" />
             </div>
         </div>
     );
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,4 +1,8 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(props);
+    
+    const {addNewTodo} = props;
+    // addNewTodo("Hihi");
     return (
         <div className="todo-new">
             <input type="text" />
