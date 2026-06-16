99012baaaccdbe1ea15ad0cac22173eb1e09cd3f
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-16 16:53:19 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,24 @@
-import './styles.css';
+import "./styles.css";
 
 const MyComponent = () => {
+    // const variable = "String";
+    // const variable = 25;
+    // const variable = true;
+    // const variable = undefined;
+    // const variable = null;
+    // const variable = [1,2,3];
+    const variable = {
+        name: "Huon",
+        age: 22,
+    };
+
     return (
         <>
-            <div> Content in Component 1</div>
-            <div className='child' style={{borderRadius: "10px"}}> Content in Component 2</div>
+            <div> {JSON.stringify(variable)} and Content in Component 1</div>
+            <div>{console.log("Huon")}</div>
+            <div className="child" style={{ borderRadius: "10px" }}>
+                Content in Component 2
+            </div>
         </>
     );
 };
