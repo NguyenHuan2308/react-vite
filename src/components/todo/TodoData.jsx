9d0c780f01df502044536c163b2867e2a4207c91
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-17 03:26:23 +0700

    video 42

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,14 +1,15 @@
 const TodoData = (props) => {
-    const {todoList} = props;
-    console.log(todoList);
-    
+    const { todoList, deleteTodo } = props;
+
     return (
         <div className="todo-data">
             {todoList.map((item, index) => {
                 return (
                     <div className="todo-item" key={item.id}>
                         <div>{item.name}</div>
-                        <button>Delete</button>
+                        <button onClick={() => deleteTodo(item.id)} style={{cursor:"pointer"}}>
+                            Delete
+                        </button>
                     </div>
                 );
             })}
