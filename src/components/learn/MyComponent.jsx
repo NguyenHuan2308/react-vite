a8240d2d866f35c84b69e2812e362265b7a5b1e2
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-16 16:11:38 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,12 @@
+import './styles.css';
+
 const MyComponent = () => {
     return (
-        <div> Content in Component 1</div>
+        <>
+            <div> Content in Component 1</div>
+            <div className='child' style={{borderRadius: "10px"}}> Content in Component 2</div>
+        </>
     );
-}
+};
 
 export default MyComponent;
