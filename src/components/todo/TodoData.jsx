e56f7be8567fc1204a05ea66151b05bf4435f34b
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-17 00:54:43 +0700

    video 39

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,13 +1,17 @@
 const TodoData = (props) => {
-    console.log(">>>> check props",props);
-    const {name,age,data} = props;
-    console.log(props);
+    const {todoList} = props;
+    console.log(todoList);
     
     return (
         <div className="todo-data">
-            <div>My name is {name}</div>
-            <div>Learning React</div>
-            <div>Watching Youtube</div>
+            {todoList.map((item, index) => {
+                return (
+                    <div className="todo-item">
+                        <div>{item.name}</div>
+                        <button>Delete</button>
+                    </div>
+                );
+            })}
             <div>
                 {JSON.stringify(props.todoList)}
             </div>
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
 
 const TodoNew = (props) => {
-    const [valueInput,setValueInput] = useState("New Text");
+    const [valueInput, setValueInput] = useState("New Text");
 
-    const {addNewTodo} = props;
+    const { addNewTodo } = props;
     // addNewTodo("Hihi");
 
     const handleClick = () => {
-        addNewTodo(valueInput)
-        
-    }
+        addNewTodo(valueInput);
+        setValueInput("");
+    };
 
     const handleOnChange = (name) => {
         setValueInput(name);
-        
-    }
+    };
     return (
         <div className="todo-new">
-            <input type="text" 
+            <input
+                type="text"
                 onChange={(e) => handleOnChange(e.target.value)}
+                value={valueInput}
             />
-            <button style={{cursor: "pointer"}}
-            onClick={handleClick}
-            >Add</button>
+            <button style={{ cursor: "pointer" }} onClick={handleClick}>
+                Add
+            </button>
             <div>My text input is = {valueInput}</div>
         </div>
     );
