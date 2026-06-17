e032c55013cc6df1223e99b4a6fa8b2058955d96
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-17 19:29:01 +0700

    video 47

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoNew from "./components/todo/TodoNew";
 import TodoData from "./components/todo/TodoData";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
 const App = () => {
     const [todoList, setTodoList] = useState([
@@ -26,27 +28,31 @@ const App = () => {
     const deleteTodo = (id) => {
         const newTodoList = todoList.filter((item) => item.id != id);
         setTodoList(newTodoList);
-    }
+    };
 
     return (
-        <div className="todo-container">
-            <div className="todo-title">Todo List</div>
-            <TodoNew addNewTodo={addNewTodo} />
-            {todoList.length > 0 ? (
-                <TodoData todoList={todoList} deleteTodo={deleteTodo}/>
-            ) : (
-                <div className="todo-image">
-                    <img src={reactLogo} alt="" className="logo" />
-                </div>
-            )}
+        <>  
+            <Header/>
+            <div className="todo-container">
+                <div className="todo-title">Todo List</div>
+                <TodoNew addNewTodo={addNewTodo} />
+                {todoList.length > 0 ? (
+                    <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+                ) : (
+                    <div className="todo-image">
+                        <img src={reactLogo} alt="" className="logo" />
+                    </div>
+                )}
 
-            {/* {todoList.length > 0 && <TodoData todoList={todoList} />}
+                {/* {todoList.length > 0 && <TodoData todoList={todoList} />}
             {todoList.length === 0 && (
                 <div className="todo-image">
                     <img src={reactLogo} alt="" className="logo" />
                 </div>
             )} */}
-        </div>
+            </div>
+            <Footer/>
+        </>
     );
 };
 
