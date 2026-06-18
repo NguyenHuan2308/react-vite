5518b787b870cb5bc0aa4400c3982ccc3d3f69da
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-19 04:12:55 +0700

    video 67

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import dns from 'dns'
 // https://vitejs.dev/config/server-options.html#server-options
 dns.setDefaultResultOrder('verbatim')
