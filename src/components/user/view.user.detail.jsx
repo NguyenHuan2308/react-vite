194a84c2c735f24c0850f66c06563582a3377964
NguyenHuan2308 <nguyenhuan45568@gmail.com>
2026-06-22 20:36:44 +0700

    video 84

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,12 +1,77 @@
-import { Button, Drawer } from "antd";
+import { Button, Drawer, notification } from "antd";
+import { useState } from "react";
+import {
+    handleUploadFile,
+    updateUserAvatarAPI,
+} from "../../services/api.service";
 
 const ViewUserDetail = (props) => {
-    const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
+    const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen ,loadUser} = props;
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+
+    const handleOnchangeFile = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(null);
+            return;
+        }
+        const file = event.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file));
+        }
+    };
+
+    const handleUpdateUserAvatar = async () => {
+        //step 1: upload file.
+        const resUpload = await handleUploadFile(selectedFile, "avatar");
+        console.log("Check resUpload: ", resUpload);
+        if (resUpload.data) {
+            //success
+            const newAvatar = resUpload.data.fileUploaded;
+
+            //step2: update user
+            const resUpdateAvatar = await updateUserAvatarAPI(
+                newAvatar,
+                dataDetail._id,
+                dataDetail.fullName,
+                dataDetail.phone,
+            );
+            console.log("Check newAvatar", newAvatar);
+            if (resUpdateAvatar.data) {
+                setIsDetailOpen(false);
+                setSelectedFile(null);
+                setPreview(null);
+                await loadUser();
+
+                notification.success({
+                    message: "Update user avatar",
+                    description: "Cập nhật avatar thành công!",
+                });
+                return;
+            } else {
+                notification.error({
+                    message: "Error update file",
+                    description: JSON.stringify(resUpdateAvatar.message),
+                });
+                return;
+            }
+        } else {
+            //fail
+            notification.error({
+                message: "Error upload file",
+                description: JSON.stringify(resUpload.message),
+            });
+            return;
+        }
+    };
 
     return (
         <>
             <Drawer
-            width={"40vw"}
+                width={"40vw"}
                 title="User Detail"
                 closable={{ "aria-label": "Close Button" }}
                 onClose={() => {
@@ -16,26 +81,85 @@ const ViewUserDetail = (props) => {
             >
                 {dataDetail ? (
                     <>
-                        <p>Id: {dataDetail._id}</p><br />
-                        <p>Full name: {dataDetail.fullName}</p><br />
-                        <p>Email: {dataDetail.email}</p><br />
-                        <p>Phone: {dataDetail.phone}</p><br />
-                        <p>Avatar: </p><br/>
-                        <div>
-                            <img width={150}
-                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} alt="" />
+                        <p>Id: {dataDetail._id}</p>
+                        <br />
+                        <p>Full name: {dataDetail.fullName}</p>
+                        <br />
+                        <p>Email: {dataDetail.email}</p>
+                        <br />
+                        <p>Phone: {dataDetail.phone}</p>
+                        <br />
+                        <p>Avatar: </p>
+                        <br />
+                        <div
+                            style={{
+                                marginTop: "10px",
+                                height: "100px",
+                                width: "150px",
+                                border: "1px solid #ccc",
+                            }}
+                        >
+                            <img
+                                style={{
+                                    height: "100%",
+                                    width: "100%",
+                                    objectFit: "contain",
+                                }}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+                                alt=""
+                            />
                         </div>
                         <div>
-                            <label htmlFor="btnUpload" style={{
-                                display:"block",
-                                width: "fit-content",
-                                marginTop: "15px",
-                                padding:"5px 10px",
-                                background: "orange",
-                                cursor: "pointer",
-                            }}>Upload Avatar</label>
-                            <input type="file" name="" hidden id="btnUpload" />
+                            <label
+                                htmlFor="btnUpload"
+                                style={{
+                                    display: "block",
+                                    width: "fit-content",
+                                    marginTop: "15px",
+                                    padding: "5px 10px",
+                                    background: "orange",
+                                    cursor: "pointer",
+                                }}
+                            >
+                                Upload Avatar
+                            </label>
+                            <input
+                                type="file"
+                                name=""
+                                hidden
+                                id="btnUpload"
+                                // onChange={handleOnchangeFile}
+                                onChange={(event) => handleOnchangeFile(event)}
+                            />
                         </div>
+                        {preview && (
+                            <>
+                                <div
+                                    style={{
+                                        marginTop: "10px",
+                                        height: "100px",
+                                        width: "150px",
+                                        marginBottom: "15px",
+                                    }}
+                                >
+                                    <img
+                                        style={{
+                                            height: "100%",
+                                            width: "100%",
+                                            objectFit: "contain",
+                                        }}
+                                        src={preview}
+                                        alt=""
+                                    />
+                                </div>
+                                <Button
+                                    type="primary"
+                                    onClick={() => handleUpdateUserAvatar()}
+                                >
+                                    Save
+                                </Button>
+                            </>
+                        )}
                     </>
                 ) : (
                     <>
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 // import axios from "axios";
 import axios from "./axios.customize";
 
-const createUserAPI = (fullName,email,password,phone) => {
+const createUserAPI = (fullName, email, password, phone) => {
     const URL_BACKEND = "/api/v1/user";
     const data = {
         fullName: fullName,
@@ -20,16 +20,43 @@ const updateUserAPI = (_id, fullName, phone) => {
         phone: phone,
     };
     return axios.put(URL_BACKEND, data);
-}
+};
 
 const deleteUserAPI = (id) => {
     const URL_BACKEND = `/api/v1/user/${id}`;
-    return axios.delete(URL_BACKEND)
-}
+    return axios.delete(URL_BACKEND);
+};
 
 const fetchAllUserAPI = () => {
     const URL_BACKEND = "/api/v1/user";
     return axios.get(URL_BACKEND);
-}
+};
 
-export { createUserAPI, updateUserAPI, fetchAllUserAPI , deleteUserAPI};
+const handleUploadFile = (file, folder) => {
+    const URL_BACKEND = `/api/v1/file/upload`;
+    let config = {
+        headers: {
+            "upload-type": folder,
+            "Content-Type": "multipart/form-data"
+        },
+    };
+    const bodyFormData = new FormData();
+    bodyFormData.append("fileImg",file);
+
+
+    return axios.post(URL_BACKEND, bodyFormData, config);
+};
+
+const updateUserAvatarAPI = (avatar,_id, fullName,phone) => {
+    const URL_BACKEND = "/api/v1/user";
+    const data = {
+        avatar: avatar,
+        _id: _id,
+        fullName: fullName,
+        phone: phone,
+    };
+    return axios.put(URL_BACKEND, data);
+};
+
+
+export { createUserAPI, updateUserAPI, fetchAllUserAPI, deleteUserAPI , handleUploadFile, updateUserAvatarAPI};
